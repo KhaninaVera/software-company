06a41ba2566287fcbaebe4139ab803e7c1667c0d
KhaninaVera <vera.khanina.work@gmail.com>
2019-11-02 12:26:49 +0200

    Добавила проект в репозиторий

---
+++ js/main.js
@@ -0,0 +1,110 @@
+// var servicesImages = $(".services_img"); 
+
+// function changeColor()
+// {
+// 	$( ".services_img" ).click(function() {
+
+// 	var ourSpan = $(this).siblings("span");
+
+//   if (ourSpan.hasClass('span_text')) {
+//   		ourSpan.removeClass('span_text')
+//   } else {
+//   		ourSpan.addClass('span_text');
+//   }
+
+// });
+// }
+
+// changeColor();
+
+// var kitty = $('h5');
+// console.log(kitty); 
+
+// function addKitty(count)
+// {
+	
+// 	for (var i = 1; i <= count; i++) {
+// 		$('.services_h1').append('<img id="kitty" src="08.jpg">');
+// 	}
+
+	
+// }
+
+// $('.services_h1').append('<img id="kitty" src="08.jpg">');
+// $('.services_h1').on('click', function() {
+
+// 	var kitty = $('#kitty');
+// 	console.log(kitty.length);
+// 	console.log($('#kitty').length);
+// 	console.log('#kitty');
+// 	console.log(!kitty);
+
+// 	if (kitty.length === 0) {
+// 		addKitty(40);
+
+// 	} else {
+// 		kitty.remove();
+// 	}
+
+
+// });
+ 
+	// if (kitty.length > 0) { // 0 > 0
+	// 	kitty.remove();
+	// } else {
+	// 	$('.services_h1').append('<img id="kitty" src="08.jpg">');
+	// }
+
+	// console.log(kitty);
+	// $('#kitty').toggle();
+
+// var doHomework = function() {
+// 	alert("Пора делать домашку!");
+// }; 
+
+// setTimeout(doHomework, 2000);
+
+
+// var counter = 555;
+// var printMessage = function() {
+// 	console.log("Ты смотришь в консоль уже " + counter + " ceк");
+// 	counter++;
+// };
+
+
+// function printM() {
+// 	console.log("Ты смотриsssшь в консоль уже " + counter + " ceк");
+// 	counter++;
+// }
+// console.log(printMessege);
+
+// var vasya = setInterval(printMessage, 1000);
+
+// if (counter === 10) {
+	// clearInterval(vasya);
+// }
+
+
+
+// $(".progect").fadeOut('1000');
+// $("h1").fadeIn('1000');
+// $(".alert").fadeOut('100');
+// $(".alert").fadeIn('100');
+// $(".detect").fadeOut('100');
+// $(".detect").fadeIn('100');
+
+// var a = ['a', 'b', 'c'];
+// a.indexof('b');
+
+// console.log(a);
+
+
+// $(document).keydown(function(event) {
+// 	if(event.which == 68) {
+// 		$('.photo').replaceWith('<img class="photo" height ="100" src="http://tomall.ru/dog/foto/b/292-n.jpg">')
+// 	}
+// 	if(event.which == 67) {
+// 		$('.photo').replaceWith('<img class="photo" height = "100" src="http://pipcat.ru/wp-content/uploads/2016/05/glazami-krasivymi-samymi-kartinki-koshki-sobaki-smeshnye-zhivotnye-kote_6175912521.jpg">')
+// 	}
+// });
+
